acaa274121e91007f740ee5cda4a3bca0b7660c1
vijayvaradbz <vijayvaradbz@gmail.com>
2024-07-17 23:06:10 +0530

    Latest change
    Latest change

--- roundupapi.py
+++ roundupapi.py
@@ -1,13 +1,15 @@
+from functools import wraps
 import hashlib
 import os
-from flask import Flask, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from bsedata.bse import BSE
 from nsetools import Nse
+import jwt
 
 b = BSE();
 b = BSE(update_codes = True);
@@ -20,6 +22,12 @@ ns = Nse()
 #         "lang": "Tamil1",
 #         "dt": datetime.now().strftime("%d/%m/%Y %H:%M:%S")      
 #     }
+
+ResploginStatus = {
+       "loginstatus": "Failed",
+       "name":"",
+       "token": ""    
+    }
 
 # addUserData = {
 #         "uid": "vj1",
@@ -35,6 +43,34 @@ def index():
 # @app.route('/stocks')
 # def getStocks():
 #     return stData
+
+
+#Common Methods
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        secret_key = "SRa1b2c3d4A1B2C3D4Roundup"
+        # Check if token is passed in the headers
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            if auth_header.startswith('Bearer '):
+                token = auth_header.split(" ")[1]  # Extract the token part
+                print(token);
+
+        if not token:
+            return "Token is missing!";
+            # return jsonify({'message': 'Token is missing!'}), 403
+
+        try:
+            data = jwt.decode(token, secret_key, algorithms=["HS256"], options={"verify_iat":False})
+            
+        except Exception as e:
+            print(str(e));
+            return str(e), 403
+
+        return f(*args, **kwargs)
+    return decorated
 
 # Insert Stock, Indices and Channel Feed data
 @app.route('/stockfeeddetails/<string:stquoteId>', methods=['POST'])
@@ -80,6 +116,7 @@ def stockfeeddetail(folderID, stquoteId, reqData):
 @app.route('/userSignUp', methods=['POST'])
 def userSignUp():
     data = request.get_data();
+    headerToken = request.authorization;
     json_object = json.loads(data); 
     json_object["signupdt"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S");
     return addUser(json_object)
@@ -87,29 +124,46 @@ def userSignUp():
 def addUser(userData):
     with open(r"COMDATA/Users.txt", 'r+') as u:
         userDataObj = json.load(u, strict=False)
-        encPass = hashlib.sha384(userDataObj["pwd"] + userDataObj["uid"]).hexdigest()
-        userDataObj["pwd"] = encPass
+        userData["pwd"] = hashlib.sha384(str(userData["uid"] + userData["pwd"]).encode()).hexdigest()
+        userData["signupdt"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         userDataObj.append(userData)
         u.seek(0)    
         json.dump(userDataObj, u, indent=4)    
-    return userDataObj
+    return "success"
 
 
-@app.route('/validateUserLogin')
+@app.route('/validateUserLogin', methods=['POST'])
 def validateUserLogin():
     data = request.get_data();
     userLoginData = json.loads(data); 
     return validateUserLoginDetails(userLoginData)
 
 def validateUserLoginDetails(userLoginData):
+    # userLoginStatus = json.loads(ResploginStatus)
     with open(r"COMDATA/Users.txt", 'r+') as f:
-        loginStatus: bool = False;
+        loginStatus = "Failed";
         userListObj = json.load(f, strict=False)
+        userPwd = hashlib.sha384(str(userLoginData["uid"] + userLoginData["pwd"]).encode()).hexdigest()
         for user in userListObj:
-            if (user["uid"] == userLoginData["uid"]) and (user["pwd"] == hashlib.sha384(userLoginData["pwd"] + userLoginData["uid"]).hexdigest()):
-                loginStatus = True;
+            if (user["uid"] == userLoginData["uid"]) and (user["pwd"] == userPwd):
+                ResploginStatus["loginstatus"] = "Success";
+                ResploginStatus["token"] = generate_auth_token(user["uid"]);
+                ResploginStatus["name"] = user["uid"];
                 break;
-    return loginStatus
+    
+    print(ResploginStatus);
+    return ResploginStatus;
+
+def generate_auth_token(userID):
+    secret_key = "SRa1b2c3d4A1B2C3D4Roundup"
+   
+    payload = {
+        "user_id": userID,
+        "exp": datetime.now() + timedelta(seconds=30),
+        "iat": datetime.now()
+    }
+    token = jwt.encode(payload, secret_key, algorithm="HS256");
+    return token
 
 def getUsers():
     with open(r"COMDATA/Users.txt", 'r+') as f:
@@ -119,19 +173,25 @@ def getUsers():
 
 #Add Channel Details
 @app.route('/addChannel', methods=['POST'])
+@token_required
 def addChannel():
     data = request.get_data();
     json_object = json.loads(data); 
-    json_object["signupdt"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S");
-    return createChannel(json_object)
+    json_object["dt"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S");
+    return createChannel(json_object, json_object["name"])
 
-def createChannel(channelData):
+def createChannel(channelData, chName):
+    
     with open(r"COMDATA/Channels.txt", 'r+') as ch:
         channelDataObj = json.load(ch, strict=False)
         channelDataObj.append(channelData)
         ch.seek(0)    
         json.dump(channelDataObj, ch, indent=4)    
         ch.truncate() 
+
+    channelData["cmt"] = "Channel Created"
+    channelData["dt"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S");
+    stockfeeddetail("CHFD", chName, channelData);
     return channelDataObj
 
 @app.route('/addChannelmembers/<string:channelId>', methods=['POST'])
@@ -169,7 +229,7 @@ def removeChannelmember(channelId, channelData):
     return channelDataObj
 
 #Get Stock feed
-@app.route('/stockfeed/<string:stquoteId>', methods = ['POST'])
+@app.route('/stockfeed/<string:stquoteId>', methods = ['GET'])
 def getStockFeedbyId(stquoteId):
     with open(r"STKFD/"+stquoteId+".txt", 'r+') as f:
         stockFeedObj = json.load(f, strict=False)
@@ -179,8 +239,6 @@ def getStockFeedbyId(stquoteId):
 def getIndicesFeedbyId(IndicesId):
     with open(r"INDSFD/"+IndicesId+".txt", 'r+') as f:
         stockFeedObj = json.load(f, strict=False)
-
-    print(stockFeedObj);
     return stockFeedObj
 
 @app.route('/channelfeed/<string:chennalId>', methods = ['GET'])
@@ -188,6 +246,10 @@ def getChannelFeedbyId(chennalId):
     with open(r"CHFD/"+chennalId+".txt", 'r+') as f:
         stockFeedObj = json.load(f, strict=False)
     return stockFeedObj
+
+@app.route('/checksite', methods = ['GET'])
+def checkSitefortesting():
+    return "API Test is Valid"
 
 @app.route('/bsestocklist')
 def getBSEStockList():
@@ -216,6 +278,7 @@ def getNSEStockList():
 
 
 #Get Live Stock Data
+
 
 
 
